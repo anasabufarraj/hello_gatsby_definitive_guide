2156f5bef02bc20eb779455a20d53d48a1bb53d9
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2022-01-20 17:48:12 +0400

    Import style in layout component

--- src/pages/components/layout.js
+++ src/pages/components/layout.js
@@ -3,10 +3,13 @@
 //------------------------------------------------------------------------------
 import React from 'react'
 import Footer from './footer'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '/static/styles/index.css'
 
 export default function Layout({ children }) {
   return (
     <div>
+      <p>Header</p>
       {children}
       <Footer />
     </div>
